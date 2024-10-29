d8458a887fbd9271894cfe03fb904f2db1716a7c
611216104001 <42273367+611216104001@users.noreply.github.com>
2024-10-29 17:47:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 #New section to display fruityvice nutrition information
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
